399ed02b674ebad5a9983a6634735e2608fd3149
Roman Kulikov <texasultra@mail.ru>
2020-10-31 00:39:17 +0300

    work in progress

--- src/Engine.php
+++ src/Engine.php
@@ -5,9 +5,21 @@ namespace Brain\Games\Engine;
 use function cli\line;
 use function cli\prompt;
 
-function runGame()
+function runGame($game)
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+
+    // Общее кол-во раундов
+    // Общее приветствие
+    // Правильный и неправльный ответ
+    // Correct!
+    // {$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}
+    // Question
+
+    $winStreak = 0;
+
+    while ($winStreak < 3) {
+    }
 }
