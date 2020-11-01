68dc812e8fbb5b0ee34db5391697b51806ef4d20
Roman Kulikov <texasultra@mail.ru>
2020-11-01 15:57:58 +0300

    work in progress-2

--- src/Engine.php
+++ src/Engine.php
@@ -4,22 +4,31 @@ namespace Brain\Games\Engine;
 
 use function cli\line;
 use function cli\prompt;
+use function Brain\Games\Even\printRules;
+use function Brain\Games\Even\addQuestion;
+use function Brain\Games\Even\calculateCorrectAnswer;
 
-function runGame($game)
+function runGame()
 {
+
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-
-    // Общее кол-во раундов
-    // Общее приветствие
-    // Правильный и неправльный ответ
-    // Correct!
-    // {$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}
-    // Question
+    printRules();
 
     $winStreak = 0;
-
     while ($winStreak < 3) {
+        $question = addQuestion();
+        $correctAnswer = calculateCorrectAnswer($question);
+        line("Question: %s", $question);
+        $answer = prompt('Your answer');
+        if ($answer === $correctAnswer) {
+            line('Correct!');
+            $winStreak++;
+        } else {
+            line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.");
+            return line("Let's try again, %s!", $name);
+        }
     }
+    return line("Congratulations, %s!", $name);
 }
--- src/games/Even.php
+++ src/games/Even.php
@@ -2,23 +2,17 @@
 
 namespace Brain\Games\Even;
 
-use function cli\line;
-use function cli\prompt;
-
-function verificationEven()
+function printRules()
 {
-    line('Answer "yes" if the number is even, otherwise answer "no".');
-    $randInt = rand(1, 99);
-    line("Question: {$randInt}");
-    $correctAnswer = $randInt % 2 === 0 ? 'yes' : 'no';
-    $answer = prompt('Your answer');
+    return 'Answer "yes" if the number is even, otherwise answer "no".';
+}
 
-    if ($answer === $correctAnswer) {
-        line('Correct!');
-        $winStreak++;
-    } else {
-        line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.");
-        return line("Let's try again, Bill!");
-    }
-    return line("Congratulations, Sam!");
+function addQuestion()
+{
+    return rand(1, 99);
+}
+
+function calculateCorrectAnswer($question)
+{
+    return $question % 2 === 0 ? 'yes' : 'no';
 }
